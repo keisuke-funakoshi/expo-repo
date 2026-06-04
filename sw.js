5d014b12be853896862c0e1fa20e8bb43258f856
keisuke-funakoshi <funakoshi7uj8@itoki.jp>
2026-06-04 18:08:24 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -2,21 +2,16 @@
 self.addEventListener('install', function(e){
   self.skipWaiting();
 });
-
 self.addEventListener('activate', function(e){
-  // 全キャッシュを削除
   e.waitUntil(
     caches.keys().then(function(keys){
-      return Promise.all(keys.map(function(k){
-        console.log('Deleting cache:', k);
-        return caches.delete(k);
-      }));
+      return Promise.all(keys.map(function(k){ return caches.delete(k); }));
     })
   );
   self.clients.claim();
 });
-
-// キャッシュせず常にネットワークから取得
 self.addEventListener('fetch', function(e){
-  e.respondWith(fetch(e.request));
+  e.respondWith(fetch(e.request).catch(function(){
+    return caches.match(e.request);
+  }));
 });
